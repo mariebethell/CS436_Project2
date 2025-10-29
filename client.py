1b19297e6bb279ffa330bf3da3f330abea08e05f
Marie Bethell <bethe009@csusm.edu>
2025-10-28 21:36:15 -0700

    added serialize, deserialize, and initial handle_request code

--- client.py
+++ client.py
@@ -5,56 +5,10 @@ import sys
 import threading
 import time
 
-
-def handle_request():
-    # Check RR table for record
-
-    # If not found, ask the local DNS server, then save the record if valid
-    local_dns_address = ("127.0.0.1", 21000)
-
-    # The format of the DNS query and response is in the project description
-
-    # Display RR table
-    pass
-
-
-def main():
-    try:
-        while True:
-            input_value = input("Enter the hostname (or type 'quit' to exit) ")
-            if input_value.lower() == "quit":
-                break
-
-            hostname = input_value
-            query_code = DNSTypes.get_type_code("A")
-
-            # For extra credit, let users decide the query type (e.g. A, AAAA, NS, CNAME)
-            # This means input_value will be two values separated by a space
-
-            handle_request()
-
-    except KeyboardInterrupt:
-        print("Keyboard interrupt received, exiting...")
-    finally:
-        # Close UDP socket
-        pass
-
-
-def serialize():
-    # Consider creating a serialize function
-    # This can help prepare data to send through the socket
-    pass
-
-
-def deserialize():
-    # Consider creating a deserialize function
-    # This can help prepare data that is received from the socket
-    pass
-
-
 class RRTable:
     def __init__(self):
-        # self.records = ?
+        self.records = []
+
         self.record_number = 0
 
         # Start the background thread
@@ -62,19 +16,48 @@ class RRTable:
         self.thread = threading.Thread(target=self.__decrement_ttl, daemon=True)
         self.thread.start()
 
-    def add_record(self):
+    def add_record(self, name, type, result, ttl, static):
         with self.lock:
-            pass
+            self.record_number += 1
 
-    def get_record(self):
+            record = {
+                "record_number": self.record_number,
+                "name": name,
+                "type": type,
+                "result": result,
+                "ttl": ttl,
+                "static": static
+            }
+
+            self.records.append(record)
+
+    def get_record(self, name, type):
         with self.lock:
-            pass
+            print("User requests:", name, type)
+            for record in self.records:
+                if record["name"] == name and record["type"] == type:
+                    return record
+            return None
 
     def display_table(self):
         with self.lock:
             # Display the table in the following format (include the column names):
             # record_number,name,type,result,ttl,static
-            pass
+
+            print("#, Name, Type, Result, TTL, Static")
+
+            for record in self.records:
+                print(
+                    f"{record['record_number']:<{10}}"
+                    f"{record['name']:<{10}}"
+                    f"{record['type']:<{10}}"
+                    f"{record['result']:<{10}}"
+                    f"{str(record['ttl']):^{10}}"
+                    f"{str(record['static']):^{10}}"
+                )
+
+            # column names
+            print("#, Name, Type, Result, TTL, Static")
 
     def __decrement_ttl(self):
         while True:
@@ -90,6 +73,112 @@ class RRTable:
         # Update record numbers
         pass
 
+    
+# Create RR table
+rr_table = RRTable()
+
+def handle_request(hostname, type):
+    # Check RR table for record
+    if rr_table.get_record(hostname, type) == None:
+        print("not found, asking local DNS server")
+
+        # If not found, ask the local DNS server, then save the record if valid
+        local_dns_address = ("127.0.0.1", 21000)
+
+        # Setup UDP connection
+        udp_connection = UDPConnection()
+
+        # Request record
+        dns_query = {
+            "transaction_id": 0,
+            "flag": "0000",
+            "question_name": hostname,
+            "question_type": type,
+            "answer_name": "",
+            "answer_type": "",
+            "ttl": "",
+            "result": ""
+        }
+
+        udp_connection.send_message(serialize(dns_query), local_dns_address)
+
+        record = deserialize(udp_connection.receive_message()[0])
+
+        rr_table.add_record(record["transaction_id"], record["answer_name"], record["answer_type"], record["result"], record["ttl"], 0)
+
+    # Display RR table
+    rr_table.display_table()
+
+
+def main():
+    try:
+        while True:
+            input_value = input("Enter the hostname (or type 'quit' to exit) ")
+            if input_value.lower() == "quit":
+                break
+
+            hostname = input_value
+            type = None # determine if user enters a type or not
+
+            query_code = DNSTypes.get_type_code("A")
+
+            # For extra credit, let users decide the query type (e.g. A, AAAA, NS, CNAME)
+            # This means input_value will be two values separated by a space
+            if " " in input_value:
+                hostname = input_value.split()[0]
+                type = input_value.split()[1]
+
+            handle_request(hostname, type)
+
+    except KeyboardInterrupt:
+        print("Keyboard interrupt received, exiting...")
+    finally:
+        # Close UDP socket
+        pass
+
+
+def serialize(dns_message):
+    # Consider creating a serialize function
+    # This can help prepare data to send through the socket
+
+    # Turn dns message dict into comma separated string to send through socket
+    serialized_message = ','.join([
+        str(dns_message["transaction_id"]),
+        str(dns_message["flag"]),
+        str(dns_message["question_name"]),
+        str(dns_message["question_type"]),
+        str(dns_message["answer_name"]),
+        str(dns_message["answer_type"]),
+        str(dns_message["ttl"]),
+        str(dns_message["result"])
+    ])
+
+    return serialized_message
+
+
+def deserialize(data):
+    # Consider creating a deserialize function
+    # This can help prepare data that is received from the socket
+    
+    # Turn string from servers back into dict
+    try:
+        dns_message_contents = data.split(',')
+
+        dns_message = {}
+
+        dns_message["flag"] = dns_message_contents[0],
+        dns_message["question_name"] = dns_message_contents[1],
+        dns_message["question_type"] = dns_message_contents[2],
+        dns_message["answer_name"] = dns_message_contents[3],
+        dns_message["answer_type"] = dns_message_contents[4],
+        dns_message["ttl"] = dns_message_contents[5],
+        dns_message["result"] = dns_message_contents[6]
+    except Exception as e:
+        # might change this handling
+        print("Corrupted dns_message returned from server")
+        exit(1)
+
+    return dns_message
 
 class DNSTypes:
     """
